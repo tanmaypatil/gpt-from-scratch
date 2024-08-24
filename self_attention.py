929a08d43e5975e4d2d8f1bd88a5307f6e66df49
Tanmay Patil <tany.patil77@gmail.com>
2024-08-24 23:26:02 +0530

    attention classes

---
+++ self_attention.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+class SelfAttention_v1(nn.Module):
+    
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key   = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, x):
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        
+        attn_scores = queries @ keys.T # omega
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+class SelfAttention_v2(nn.Module):
+    
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+
+        context_vec = attn_weights @ values
+        return context_vec
+    
+class CausalAttention(nn.Module):
+    
+    def __init__(self, d_in, d_out, context_length,
+                 dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout) # New
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1)) # New
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape # New batch dimension b
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2) # Changed transpose
+        attn_scores.masked_fill_(  # New, _ ops are in-place
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)  # `:num_tokens` to account for cases where the number of tokens in the batch is smaller than the supported context_size
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        attn_weights = self.dropout(attn_weights) # New
+
+        context_vec = attn_weights @ values
+        return context_vec
---
+++ test/test_attention.py
@@ -0,0 +1,35 @@
+from self_attention import *
+d_in = 3
+d_out = 2
+inputs = torch.tensor(
+        [[0.43, 0.15, 0.89],  # Your     (x^1)
+         [0.55, 0.87, 0.66],  # journey  (x^2)
+         [0.57, 0.85, 0.64],  # starts   (x^3)
+         [0.22, 0.58, 0.33],  # with     (x^4)
+         [0.77, 0.25, 0.10],  # one      (x^5)
+         [0.05, 0.80, 0.55]]  # step     (x^6)
+    )
+
+def test_attn():
+    torch.manual_seed(123)
+    sa_v1 = SelfAttention_v1(d_in, d_out)
+    z = (sa_v1(inputs))
+    assert z.shape == (6,2)
+    print(z)
+    
+def test_attention2():
+    torch.manual_seed(789)
+    sa_v2 = SelfAttention_v2(d_in, d_out)
+    z = (sa_v2(inputs))
+    assert z.shape == (6,2)
+    print(z)
+    
+def test_casual_Attn():
+    torch.manual_seed(123)
+    batch = torch.stack((inputs, inputs), dim=0)
+    context_length = batch.shape[1]
+    ca = CausalAttention(d_in, d_out, context_length, 0.0)
+    z = (ca(batch))
+    
+    print(z)
+
